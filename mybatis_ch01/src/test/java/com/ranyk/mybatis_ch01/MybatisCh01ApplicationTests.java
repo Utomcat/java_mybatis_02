5641539558890d81c792cd44d8ab842a5831dd0d
ranyk <1390851149@qq.com>
2020-09-03 23:34:58 +0800

    增加程序的异常捕获处理验证，验证结果和程序见MybatisCh01ApplicationTest.java中的method3

--- mybatis_ch01/src/test/java/com/ranyk/mybatis_ch01/MybatisCh01ApplicationTests.java
+++ mybatis_ch01/src/test/java/com/ranyk/mybatis_ch01/MybatisCh01ApplicationTests.java
@@ -28,7 +28,7 @@ class MybatisCh01ApplicationTests {
     void test00() {
         String a = "AAAAA BBA AA";
         char b = ' ';
-        log.info("在字符串 "+a+" 中出现字符 "+b+" 的次数为："+StringOperate.statisticsStr1(a, b));
+        log.info("在字符串 " + a + " 中出现字符 " + b + " 的次数为：" + StringOperate.statisticsStr1(a, b));
     }
 
     @Test
@@ -39,11 +39,11 @@ class MybatisCh01ApplicationTests {
         Map<String, Integer> map = StringOperate.statisticsStr2(a);
 
 
-        log.info("在字符串 "+a+" 中出现字符 "+b+" 的次数为："+map.get("A"));
+        log.info("在字符串 " + a + " 中出现字符 " + b + " 的次数为：" + map.get("A"));
     }
 
     @Test
-    void test02(){
+    void test02() {
         String a = "AAAAA BBA AA";
         String b = "boo:and:foo";
         //获取字符串从指定位置开始到这个字符串结尾的子串
@@ -71,7 +71,7 @@ class MybatisCh01ApplicationTests {
 
 
     @Test
-    void test03(){
+    void test03() {
         String a = "AAAAA BBA AA中文汉字";
         String b = "boo:and:foo";
         Map<String, Integer> stringIntegerMap = StringOperate.statisticsStr2(a);
@@ -82,22 +82,20 @@ class MybatisCh01ApplicationTests {
         String[] split = a.split("");
 
         System.out.println("当前字符串中各个字符的个数为");
-        if (num == split.length){
+        if (num == split.length) {
             System.out.println("所有的字符都只有一个：");
             System.out.println(a);
-        }else {
+        } else {
             Iterator<Map.Entry<String, Integer>> iterator = stringIntegerMap.entrySet().iterator();
-            while (iterator.hasNext()){
+            while (iterator.hasNext()) {
                 Map.Entry<String, Integer> next = iterator.next();
-                if (" ".equals(next.getKey())){
-                    System.out.print("字符：空格 的个数为："+next.getValue()+" ");
-                }else {
-                    System.out.print("字符："+next.getKey()+" 的个数为："+next.getValue()+" ");
+                if (" ".equals(next.getKey())) {
+                    System.out.print("字符：空格 的个数为：" + next.getValue() + " ");
+                } else {
+                    System.out.print("字符：" + next.getKey() + " 的个数为：" + next.getValue() + " ");
                 }
             }
         }
-
-
 
 
     }
@@ -108,7 +106,7 @@ class MybatisCh01ApplicationTests {
      * 结果 实体对象为 null 且对实体对象进行的属性值的获取会出现 空指针
      */
     @Test
-    public void method0(){
+    public void method0() {
         Tb2 tb2 = tbService.queryById("8");
         System.out.println(tb2.toString());
 
@@ -118,13 +116,13 @@ class MybatisCh01ApplicationTests {
      * 空值处理验证一
      */
     @Test
-    public void method1(){
+    public void method1() {
 
         for (int i = 0; i < 12; i++) {
 
             Tb2 tb2 = tbService.queryById(String.valueOf(i));
 
-            if (null == tb2){
+            if (null == tb2) {
                 log.error("未查询到数据");
                 continue;
             }
@@ -140,10 +138,36 @@ class MybatisCh01ApplicationTests {
      * 空值处理验证二
      */
     @Test
-    public void method2(){
+    public void method2() {
         List<Object> objects = tbService.queryNullFromDual();
         for (Object object : objects) {
             System.out.println(object);
+        }
+    }
+
+
+    /**
+     * 验证 try catch 之后,在catch中不进行异常的抛出,仅输出错误行时, 程序是否能够继续执行
+     * 验证结果: 不抛出异常,程序继续执行,直至程序结束
+     */
+    @Test
+    public void method3() {
+        int a = 1;
+        for (int i = 0; i < 100; i++) {
+
+            if (a > 10){
+                a = 1;
+            }
+
+            try {
+                Tb2 tb2 = tbService.queryById(String.valueOf(a));
+                log.info( "查询出来的结果为: " + tb2.toString());
+            }catch (Exception e){
+                log.error( "错误信息为: " + e.getMessage() + "\r\n 报错行为: " + e.getStackTrace()[0].getLineNumber());
+            }finally {
+                log.info("这是第 " + i + " 次的执行结果");
+                a++;
+            }
         }
     }
 
