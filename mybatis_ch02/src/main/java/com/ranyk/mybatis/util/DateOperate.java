49b8ef421dcf594c7d160480f60520e9bbbe7437
ranyk <1390851149@qq.com>
2021-03-01 22:43:21 +0800

    增加单元测试;增加时间处理工具类

---
+++ mybatis_ch02/src/main/java/com/ranyk/mybatis/util/DateOperate.java
@@ -0,0 +1,14 @@
+package com.ranyk.mybatis.util;
+
+/**
+ * CLASS_NAME: DateOperate<br/>
+ * Description: 时间处理工具类<br/>
+ *
+ * @author ranYk <br/>
+ * @version 1.0
+ */
+public class DateOperate {
+
+
+
+}
--- mybatis_ch02/src/test/java/com/ranyk/mybatis/StringOperateTest.java
+++ mybatis_ch02/src/test/java/com/ranyk/mybatis/StringOperateTest.java
@@ -1,9 +1,14 @@
 package com.ranyk.mybatis;
 
+import com.ranyk.mybatis.excel.vo.ComplexHeadData;
 import com.ranyk.mybatis.util.StringOperate;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.text.MessageFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.regex.Pattern;
 
 /**
@@ -20,28 +25,28 @@ class StringOperateTest {
 
     @Test
     void getAge() {
-        log.info("获取的年纪为: {}",StringOperate.getAge(96,12,16));
+        log.info("获取的年纪为: {}", StringOperate.getAge(96, 12, 16));
     }
 
     @Test
-    void getPersonInfoByIdCard(){
-        log.info("获取到的个人信息为: {}",StringOperate.getPersonInfoByIdCard("510824199612161637"));
+    void getPersonInfoByIdCard() {
+        log.info("获取到的个人信息为: {}", StringOperate.getPersonInfoByIdCard("510824199612161637"));
     }
 
     @Test
-    void test01(){
-        log.info("number: {}",(2<<15)%11);
-    }
-
-
-    @Test
-    void test02(){
-        boolean valid = StringOperate.verifyValid("510824199612161637",15);
+    void test01() {
+        log.info("number: {}", (2 << 15) % 11);
     }
 
 
     @Test
-    void test03(){
+    void test02() {
+        boolean valid = StringOperate.verifyValid("510824199612161637", 15);
+    }
+
+
+    @Test
+    void test03() {
         String x = "12333";
         String y = "0";
         String z = "3";
@@ -49,14 +54,29 @@ class StringOperateTest {
         String id = "510824961216163";
         String id2 = "510824961216163xx";
 
-        log.info("{}",id2.toUpperCase());
+        log.info("{}", id2.toUpperCase());
 
-        if (Pattern.matches(reg,id)) {
+        if (Pattern.matches(reg, id)) {
             log.info("匹配成功!");
-        }else {
+        } else {
             log.info("匹配失败!");
         }
 
     }
+
+    @Test
+    void test04() {
+        log.info(" 当前的时间为:  {}", LocalDate.now().toString() + LocalDateTime.now().toString());
+        log.info(" ComplexHeadData class 对象调用getName方法后的结果:  {}", ComplexHeadData.class.getName().substring(ComplexHeadData.class.getName().lastIndexOf(".") + 1));
+
+    }
+
+
+    @Test
+    void test05(){
+        String dateTime = MessageFormat.format("{0,date,yyyy-MM-dd HH:mm:ss:ms}" , new Object[]{new Date(System.currentTimeMillis())});
+        log.info("格式化时间 {}",dateTime);
+    }
+
 
 }
